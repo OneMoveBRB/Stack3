d23b88e7151b476d9e07312f750f0d727f394414
OneMoveBRB <sh_amir2014@mail.ru>
2025-10-03 16:21:06 +0300

    Update stack_functionality.cpp

--- stack_functionality.cpp
+++ stack_functionality.cpp
@@ -2,37 +2,31 @@
 
 #include <stdio.h>
 
+
 StackErr_t StackPush(Stack_t* stack, type value) {
-    if (stack == NULL) return STACK_VERIFY(stack, EFAULT);
-
 #if STACK_MODE == STACK_DEBUG
-    STACK_VERIFY(stack, errno);
+    StackErr_t err_flag = OK;
+    if ((err_flag = STACK_VERIFY(stack, errno)) != OK) return err_flag;
 #endif
 
-// Сделаю макрос TWO_CANARIES 2 ....
-// Тогда if (stack->size + 1 == stack->capacity - TWO_CANARIES) StackUpdate(stack); без #if
-#if STACK_MODE == STACK_DEBUG
-    if (stack->size + 1 == stack->capacity - 2) StackUpdate(stack);
-#else
-    if (stack->size + 1 == stack->capacity) StackUpdate(stack);
-#endif
+    if (stack->size + 1 == stack->capacity - TWO_CANARIES) StackUpdate(stack);
 
     *(type*)MoveArrPtr(stack->arr, stack->size) = value;
     ++stack->size;
 
 #if STACK_MODE == STACK_DEBUG
-    STACK_VERIFY(stack, errno);
+    if ((err_flag = STACK_VERIFY(stack, errno)) != OK) return err_flag;
 #endif
 
     return OK;
 }
 
 type StackPop(Stack_t* stack) {
-    if (stack == NULL) return STACK_VERIFY(stack, EFAULT);
-    if (stack->size == 0) return STACK_VERIFY(stack, EPERM);
-
 #if STACK_MODE == STACK_DEBUG
-    STACK_VERIFY(stack, errno);
+    StackErr_t err_flag = OK;
+    if ((err_flag = STACK_VERIFY(stack, errno)) != OK) return err_flag;
+
+    if (stack->size == 0) return STACK_VERIFY(stack, EPERM);
 #endif
 
     type element = *(type*)MoveArrPtr(stack->arr, stack->size-1);
@@ -40,7 +34,7 @@ type StackPop(Stack_t* stack) {
     --stack->size;
 
 #if STACK_MODE == STACK_DEBUG
-    STACK_VERIFY(stack, errno);
+    if ((err_flag = STACK_VERIFY(stack, errno)) != OK) return err_flag;
 #endif
     
     return element;
